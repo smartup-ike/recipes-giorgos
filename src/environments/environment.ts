468a07cf9e9b10fe3a747a92b4ed0c41311bf615
giotsere <giorgitsereteli@protonmail.com>
2023-04-12 13:25:15 +0300

    firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyBP7-SvXvYjHf8NoqLgmza-dsZ27gz_vqc',
-    authDomain: 'web-fb-1bc18.firebaseapp.com',
-    projectId: 'web-fb-1bc18',
-    storageBucket: 'web-fb-1bc18.appspot.com',
-    messagingSenderId: '460798883331',
-    appId: '1:460798883331:web:179c4e3ddaee6ab50d4036',
-    databaseURL:
-      'https://web-fb-1bc18-default-rtdb.europe-west1.firebasedatabase.app',
+    apiKey: 'AIzaSyAwkHHEBZnM1j0fHMxG4EniZDDifxh_kso',
+    authDomain: 'smartup-hr-test-frontend.firebaseapp.com',
+    databaseURL: 'https://smartup-hr-test-frontend.firebaseio.com',
+    projectId: 'smartup-hr-test-frontend',
+    storageBucket: 'smartup-hr-test-frontend.appspot.com',
+    messagingSenderId: '566229714425',
+    appId: '1:566229714425:web:9ffcd8b621f7016da2a928',
+    measurementId: 'G-X895KERW0W',
   },
 };
