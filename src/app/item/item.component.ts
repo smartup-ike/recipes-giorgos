ca811070dc3abc54f936148fda7c2051bf6b10c2
giotsere <giorgitsereteli@protonmail.com>
2023-04-12 13:20:01 +0300

    pass item to item-detail comp

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
+import { ItemSummary } from '../models/item-summary.model';
 
 @Component({
   selector: 'app-item',
@@ -6,16 +8,23 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./item.component.css'],
 })
 export class ItemComponent implements OnInit {
+  constructor(private data: DataService) {}
+
   isHidden = true;
 
   @Input() id: string = '';
   @Input() title: string = '';
   @Input() imageUrl: string = '';
   @Input() description: string = '';
+  @Input() item: ItemSummary | undefined;
   url: string = '';
 
   setHidden(e: Event) {
     this.isHidden = !this.isHidden;
+  }
+
+  changeItem(item: ItemSummary) {
+    this.data.changeItem(item);
   }
 
   ngOnInit(): void {
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,16 +1,29 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { ItemSummary } from '../models/item-summary.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
   private id = new BehaviorSubject('Επιλεξε μενου');
+  private path = new BehaviorSubject('');
+  private item = new BehaviorSubject({ id: '', title: '' });
   currentID = this.id.asObservable();
+  currentPath = this.path.asObservable();
+  currentItem = this.item.asObservable();
 
   constructor() {}
 
   changeID(id: string) {
     this.id.next(id);
+  }
+
+  changePath(path: string) {
+    this.path.next(path);
+  }
+
+  changeItem(item: ItemSummary) {
+    this.item.next(item);
   }
 }
